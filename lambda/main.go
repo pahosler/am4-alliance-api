bf86b2fc965f7d2ca3c0f6a9eb1d30329b1276e4
pahosler <pahosler@gmail.com>
2020-06-30 14:46:27 -0500

    added headers

--- lambda/main.go
+++ lambda/main.go
@@ -2,24 +2,24 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 	"io/ioutil"
 	"net/http"
 )
 
-func getAlliance() []byte {
+func getAlliance() string {
 	var token = "KJWRFSERGWerDSWFWeoriwoWODESTRgSWDF:456787654Vjhved"
 	var alliance = "tiger-team1"
-	var am4 []byte
+	var am4 = ""
 	response, err := http.Get("https://airline4.net/api?access_token=" + token + "&search=" + alliance)
 
 	if err != nil {
-		e := `{ "error":  err}`
-		am4 = []byte(e)
+		am4 = fmt.Sprintf("There was an error, %s", err)
 	} else {
 		data, _ := ioutil.ReadAll(response.Body)
-		am4 = []byte(data)
+		am4 = string(data)
 	}
 
 	return am4
@@ -30,6 +30,11 @@ func handler(ctx context.Context, request events.APIGatewayProxyRequest) (*event
 	return &events.APIGatewayProxyResponse{
 		StatusCode: 200,
 		Body:       res,
+		Headers: {
+			"Access-Control-Allow-Origin":  "*",
+			"Access-Control-Allow-Headers": "Content-Type",
+			"Access-Control-Allow-Headers": "application/json",
+		},
 	}, nil
 }
 
