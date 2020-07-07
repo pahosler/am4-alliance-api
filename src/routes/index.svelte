6f45cfe44ef55805f35adce43d99fa1289557747
Paul Hosler <pahosler@gmail.com>
2020-07-07 13:15:57 -0500

    mostly harmless

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,13 +1,43 @@
 <script>
   import { onMount } from "svelte";
-  import res from '../../static/start.json';
-  import getFauna from './_getFauna.js';
+  import res from "../../static/start.json";
+  // import getFauna from './_getFauna.js';
 
-  let members
-  let current
+  let members;
+  let current;
   onMount(async () => {
+    const faunadb = window.faunadb;
+    const q = faunadb.query;
+    const client = new faunadb.Client({
+      secret: "fnADvsSZphACAJMZtbFfkj4b83lYv_oQQGQRCyce",
+      domain: "db.fauna.com",
+      scheme: "https"
+    });
+
+    try {
+      let { data } = await client.query(
+        q.Get(q.Ref(q.Collection("team"), "270057444654187008"))
+      );
+      let { members } = data;
+      current = await members.sort((a, b) => {
+        let nameA = a.company.toUpperCase();
+        let nameB = b.company.toUpperCase();
+        if (nameA < nameB) {
+          return -1;
+        }
+        if (nameA > nameB) {
+          return 1;
+        }
+        return 0;
+      });
+      //return current;
+      console.log("current: " + current.length);
+    } catch (err) {
+      console.log(err);
+    }
+
     const data = await JSON.parse(JSON.stringify(res));
-    current = await getFauna(() => current)
+    //current = await getFauna(() => current);
     members = await data.members.sort((a, b) => {
       let nameA = a.company.toUpperCase();
       let nameB = b.company.toUpperCase();
@@ -19,9 +49,11 @@
       }
       return 0;
     });
+    console.log(members.length);
     window.scrollTo(0, 0);
-  })
+  });
 </script>
+
 <style>
   p {
     margin: 1em auto;
@@ -48,15 +80,29 @@
         </tr>
       </thead>
       <tbody>
-        {#each members as member,i}
+        {#each members as member, i}
           <tr>
             <th>{member.company}</th>
             <td>${member.shareValue}</td>
             <td>{member.flights}</td>
-            <td>{member.contributed.toLocaleString('en-US',{style: 'currency', currency: 'USD',})}</td>
-            <td>{current[i].contributed.toLocaleString('en-US',{style: 'currency', currency: 'USD',})}</td>
-            <td>{((current[i].contributed - member.contributed)/member.contributed)}</td>
-            <td>{((current[i].contributed - member.contributed)/current[i].contributed)*100}%</td>
+            <td>
+              {member.contributed.toLocaleString('en-US', {
+                style: 'currency',
+                currency: 'USD'
+              })}
+            </td>
+            <td>
+              {current[i].contributed.toLocaleString('en-US', {
+                style: 'currency',
+                currency: 'USD'
+              })}
+            </td>
+            <td>
+              {((current[i].contributed - member.contributed) / member.contributed).toFixed(2)}
+            </td>
+            <td>
+              {Math.floor(((current[i].contributed - member.contributed) / current[i].contributed) * 100)}%
+            </td>
           </tr>
         {/each}
       </tbody>
--- src/server.js
+++ src/server.js
@@ -1,22 +1,22 @@
-import sirv from 'sirv';
-import polka from 'polka';
-import compression from 'compression';
-import * as sapper from '@sapper/server';
-import fs from 'fs';
-import path from 'path';
-import fetch from 'node-fetch';
-import faunadb from 'faunadb';
+import sirv from 'sirv'
+import polka from 'polka'
+import compression from 'compression'
+import * as sapper from '@sapper/server'
+import fs from 'fs'
+import path from 'path'
+import fetch from 'node-fetch'
+import faunadb from 'faunadb'
 
-const q = faunadb.query;
+const q = faunadb.query
 const client = new faunadb.Client({
   secret: 'fnADvsSZphACAJMZtbFfkj4b83lYv_oQQGQRCyce',
-});
+})
 
 const getData = () => {
   fetch('https://am4-api.netlify.app/.netlify/functions/am4-alliance-api', {})
     .then((res) => res.json())
-    .then(({ members }) => writeData(members));
-};
+    .then(({ members }) => writeData(members))
+}
 
 const writeData = (team) => {
   client
@@ -27,22 +27,18 @@ const writeData = (team) => {
         },
       })
     )
-    .catch((err) => console.log(err));
-};
+    .catch((err) => console.log(err))
+}
 
-const { PORT, NODE_ENV } = process.env;
-const dev = NODE_ENV === 'development';
+const { PORT, NODE_ENV } = process.env
+const dev = NODE_ENV === 'development'
 
 polka() // You can also use Express
-  .use(
-    compression({ threshold: 0 }),
-    sirv('static', { dev }),
-    sapper.middleware()
-  )
+  .use(compression({ threshold: 0 }), sirv('static', { dev }), sapper.middleware())
   .listen(PORT, (err) => {
-    if (err) console.log('error', err);
-  });
+    if (err) console.log('error', err)
+  })
 
 setInterval(() => {
-  getData();
-}, 10000 * 60);
+  getData()
+}, 3000 * 60)
